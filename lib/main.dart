0e782ff5b34cf11ad6e55315559a285d84558291
Adnan Hakim <adnanhakim007@gmail.com>
2020-03-26 14:44:59 +0530

    Initial

---
+++ lib/main.dart
@@ -0,0 +1,21 @@
+import 'package:flutter/material.dart';
+import 'package:travel_app/screens/home_screen.dart';
+
+void main() => runApp(MyApp());
+
+class MyApp extends StatelessWidget {
+  // This widget is the root of your application.
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'Flutter Travel UI',
+      theme: ThemeData(
+        primaryColor: Color(0xFF3EBACE),
+        accentColor: Color(0xFFD8ECF1),
+        scaffoldBackgroundColor: Color(0xFFF3F5F7),
+        fontFamily: 'Muli',
+      ),
+      home: HomeScreen(),
+    );
+  }
+}
